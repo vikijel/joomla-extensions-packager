a7ee6cff0f848259a7930290a4cdc90f6fd3cc9e
Viktor Jelínek <vikijel@gmail.com>
2016-05-20 01:11:14 +0200

    devel...

--- src/UpdateServer.php
+++ src/UpdateServer.php
@@ -7,5 +7,111 @@ namespace VikiJel\JoomlaExtensionsPackager;
 
 class UpdateServer
 {
+	/**
+	 * @var string URL of update server's xml list of updates
+	 */
+	protected $url;
 
+	/**
+	 * @var string Name of update server
+	 */
+	protected $name;
+
+	/**
+	 * @var string Type of update server
+	 */
+	protected $type;
+
+	/**
+	 * @var int Priority of update server
+	 */
+	protected $priority;
+
+	public function __construct($url, $name = '', $type = '', $priority = 1)
+	{
+		$this->setUrl($url);
+		$this->setName($name);
+		$this->setType($type);
+		$this->setPriority($priority);
+	}
+
+	/**
+	 * @return string URL of update server's xml list of updates
+	 */
+	public function getUrl()
+	{
+		return $this->url;
+	}
+
+	/**
+	 * @param string $url URL of update server's xml list of updates
+	 *
+	 * @return UpdateServer
+	 */
+	public function setUrl($url)
+	{
+		$this->url = $url;
+
+		return $this;
+	}
+
+	/**
+	 * @return string Name of update server
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name Name of update server
+	 *
+	 * @return UpdateServer
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
+
+	/**
+	 * @return string Type of update server
+	 */
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	/**
+	 * @param string $type Type of update server
+	 *
+	 * @return UpdateServer
+	 */
+	public function setType($type)
+	{
+		$this->type = $type;
+
+		return $this;
+	}
+
+	/**
+	 * @return int Priority of update server
+	 */
+	public function getPriority()
+	{
+		return $this->priority;
+	}
+
+	/**
+	 * @param int $priority Priority of update server
+	 *
+	 * @return UpdateServer
+	 */
+	public function setPriority($priority)
+	{
+		$this->priority = $priority;
+
+		return $this;
+	}
 }
--- tests/UpdateServerTest.php
+++ tests/UpdateServerTest.php
@@ -7,9 +7,14 @@ namespace VikiJel\JoomlaExtensionsPackager;
 
 class UpdateServerTest extends \PHPUnit_Framework_TestCase
 {
+	static $url      = 'http://updates.vikijel.cz/test.xml';
+	static $name     = 'VikiJel';
+	static $type     = 'vikijel';
+	static $priority = 2;
+
 	public function testClassInstantiates()
 	{
-		$instance = new UpdateServer();
+		$instance = new UpdateServer(static::$url, static::$name, static::$type, static::$priority);
 
 		$this->assertInstanceOf('\\VikiJel\\JoomlaExtensionsPackager\\UpdateServer', $instance);
 	}
