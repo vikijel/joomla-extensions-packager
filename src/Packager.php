715cbb759449755fd329c8b8091cb8c9a7fe02d5
Viktor Jelínek <vikijel@gmail.com>
2016-05-20 16:41:30 +0200

    devel...

--- src/Packager.php
+++ src/Packager.php
@@ -1,23 +1,36 @@
 <?php
 namespace VikiJel\JoomlaExtensionsPackager;
 
+use ZipArchive;
+
 class Packager
 {
 	protected static $default_target_dir = __DIR__ . '..' . DIRECTORY_SEPARATOR . 'out';
 
-	public static function pack(Package $package, $target_dir = null)
+	/**
+	 * @param Package $package
+	 * @param string  $dir  Location of target file (defaults to ../out)
+	 * @param string  $file Filename of target file (defaults to auto-generate from package name)
+	 *
+	 * @return bool
+	 * @throws \Exception
+	 */
+	public static function pack(Package $package, $dir = null, $file = null)
 	{
 		$package->prepare();
 
-		$target_dir   = empty($target_dir) ? self::$default_target_dir : $target_dir;
-		$manifest_xml = $package->getPkgXml(true);
+		$file = empty($file) ? $package->getPkgPrefix() . $package->getPkgName() . '-' . $package->getVersion() . '.zip' : $file;
+		$dir  = empty($dir) ? self::$default_target_dir : $dir;
+		$path = $dir . DIRECTORY_SEPARATOR . $file;
+		$xml  = $package->getPkgXml(true);
+		$zip  = new ZipArchive();
 
 		//todo
 		throw new \Exception(
 			"TODO:\n" .
-			"target_dir: " . $target_dir . "\n" .
-			"manifest_xml: " . var_export($manifest_xml, true) .
-			"package: " . print_r($package, true)
+			"Target path: " . $path . "\n" .
+			"Xml: " . var_export($xml, true) .
+			"Package: " . print_r($package, true)
 		);
 
 		return true;
